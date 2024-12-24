3216eca52e1fdf5190c73e5db1f85f08443f0224
Raginichourasia <raginichourasia12@gmail.com>
2024-12-25 02:20:58 +0530

    Added test for single number returning itself

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Calculator do
   it 'returns 0 for an empty string' do
     expect(MyCalculator.add("")).to eq(0)
   end
+
+  it 'returns the number itself for a single number string' do
+    expect(MyCalculator.add("1")).to eq(1)
+  end
 end
 
