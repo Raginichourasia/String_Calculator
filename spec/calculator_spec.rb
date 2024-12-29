6ecb47d26d4cd0c7de9695a7bee896d437c2f982
Raginichourasia <raginichourasia12@gmail.com>
2024-12-29 17:42:29 +0530

    Added test for newline delimiter

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe Calculator do
   it 'returns the sum of multiple numbers' do
     expect(MyCalculator.add("1,2,3,4")).to eq(10)
   end
+
+  it 'handles newlines as delimiters' do
+    expect(MyCalculator.add("1\n2,3")).to eq(6)
+  end
 end
 
