2a1f491965460613857a893d5956b46143163481
Raginichourasia <raginichourasia12@gmail.com>
2024-12-30 01:20:24 +0530

    Refactor code

--- calculator.rb
+++ calculator.rb
@@ -3,8 +3,8 @@ class MyCalculator
     return 0 if input.strip.empty?
 
     numbers = parse_input(input)
-    negative_numbers = numbers.select { |n| n < 0 }
-    
+    negative_numbers = numbers.select(&:negative?)
+
     if negative_numbers.any?
       raise "negative numbers not allowed: #{negative_numbers.join(', ')}"
     end
