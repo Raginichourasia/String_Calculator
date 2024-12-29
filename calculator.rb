5107bf30eb748c94de868e7416f6692920f16bd7
Raginichourasia <raginichourasia12@gmail.com>
2024-12-29 23:46:14 +0530

    Implemented handling for multiple spaces between numbers

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@ class MyCalculator
   def self.add(input)
     return 0 if input.strip.empty?
 
+    input = input.gsub(' ', '') # Remove all spaces from input
     numbers = parse_input(input)
     numbers.sum
   end
@@ -11,7 +12,7 @@ class MyCalculator
   def self.parse_input(input)
     if input.start_with?("//")
       delimiter, numbers = input[2..].split("\n", 2)
-      numbers.split(delimiter).map(&:to_i)
+      numbers.split(/[\n,#{delimiter}]/).map(&:to_i)
     else
       input.gsub("\n", ',').split(',').map(&:to_i)
     end
